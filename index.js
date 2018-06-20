fbf5e5ddc139a1d0614d000bcf9e92cccd56a6f4
sanky1984@gmail.com <sanky1984@gmail.com>
2018-06-20 19:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!'"
 }
 
-function holidayCountdown
+function holidayCountdown(days, holiday) {
+  return "It's ${days} days until ${holiday}!'"
+}
+
